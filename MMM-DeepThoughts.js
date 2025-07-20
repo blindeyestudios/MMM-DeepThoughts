48a535d86772cbcf22b390f4c399131ad195c517
blindeyestudios <blindeyestudios.aaron@gmail.com>
2025-07-20 14:16:05 -0400

    Styling

--- MMM-DeepThoughts.js
+++ MMM-DeepThoughts.js
@@ -1,44 +1,51 @@
 Module.register("MMM-DeepThought", {
-  defaults: {
-    updateInterval: 60 * 60 * 1000, // 1 hour
-  },
+    defaults: {
+        updateInterval: 60 * 60 * 1000, // 1 hour
+    },
 
-  start() {
-    this.inspiration = "Loading inspiration...";
-    this.weirdThought = "Loading weirdness...";
-    this.getThoughts();
-    this.scheduleUpdate();
-  },
+    getStyles: function () {
+        return ["MMM-DeepThoughts.css"];
+    },
 
-  scheduleUpdate() {
-    setInterval(() => {
-      this.getThoughts();
-    }, this.config.updateInterval);
-  },
+    start() {
+        this.inspiration = "Loading inspiration...";
+        this.weirdThought = "Loading weirdness...";
+        this.getThoughts();
+        this.scheduleUpdate();
+    },
 
-  getThoughts() {
-    this.sendSocketNotification("GET_THOUGHTS");
-  },
+    scheduleUpdate() {
+        setInterval(() => {
+            this.getThoughts();
+        }, this.config.updateInterval);
+    },
 
-  socketNotificationReceived(notification, payload) {
-    if (notification === "THOUGHTS") {
-      this.inspiration = payload.inspiration;
-      this.weirdThought = payload.weirdThought;
-      this.updateDom();
+    getThoughts() {
+        this.sendSocketNotification("GET_THOUGHTS");
+    },
+
+    socketNotificationReceived(notification, payload) {
+        if (notification === "THOUGHTS") {
+            this.inspiration = payload.inspiration;
+            this.weirdThought = payload.weirdThought;
+            this.updateDom();
+        }
+    },
+
+    getDom() {
+        const wrapper = document.createElement("div");
+        wrapper.className = "deep-thoughts";
+
+        const inspire = document.createElement("div");
+        inspire.innerHTML = `<strong>Inspiration:</strong> ${this.inspiration}`;
+        inspire.className = "inspirational";
+
+        const weird = document.createElement("div");
+        weird.innerHTML = `<strong>Weird Thought:</strong> ${this.weirdThought}`;
+        weird.className = "weird";
+
+        wrapper.appendChild(inspire);
+        wrapper.appendChild(weird);
+        return wrapper;
     }
-  },
-
-  getDom() {
-    const wrapper = document.createElement("div");
-
-    const inspire = document.createElement("div");
-    inspire.innerHTML = `<strong>Inspiration:</strong> ${this.inspiration}`;
-
-    const weird = document.createElement("div");
-    weird.innerHTML = `<strong>Weird Thought:</strong> ${this.weirdThought}`;
-
-    wrapper.appendChild(inspire);
-    wrapper.appendChild(weird);
-    return wrapper;
-  }
 });
